79d4d0400d144fcedf62fbd62d7c9e418fe3fcda
ravindra.bhatt <ravindra.bhatt@rakuten.com>
2020-05-05 13:06:05 +0900

    Add password input with password length validation error message

--- src/screens/InputScreen.js
+++ src/screens/InputScreen.js
@@ -3,9 +3,10 @@ import { Text, View, StyleSheet, TextInput } from 'react-native';
 
 const InputScreen = () => {
     const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
 
     return <View>
-                <Text>Enter Name</Text>
+                <Text style={styles.text}>Enter Name</Text>
                 <TextInput 
                     style={styles.input}
                     autoCapitalize="none"
@@ -13,7 +14,17 @@ const InputScreen = () => {
                     value={name}
                     onChangeText={(newValue) => setName(newValue)}
                 />
-                <Text>My name is {name}</Text>
+                <Text style={styles.text}>My name is {name}</Text>
+
+                <Text style={styles.text}>Enter Password:</Text>
+                <TextInput 
+                    style={styles.input}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={password}
+                    onChangeText={(newValue) => setPassword(newValue)}
+                />
+                {password.length <= 5  && password.length > 0 ? <Text style = {styles.error}>Password must be longer than 5 characters </Text> : null}
             </View>
 };
 
@@ -22,6 +33,14 @@ const styles = StyleSheet.create({
         margin: 15,
         borderColor: 'black',
         borderWidth: 1
+    },
+    
+    text: {
+        marginTop: 15,
+    },
+    
+    error: {
+        color: 'red'
     }
 });
 
