5d3ce2dba7b72d77df7c15255374094d8c6dc029
ravindra.bhatt <ravindra.bhatt@rakuten.com>
2020-05-05 11:08:54 +0900

    Add input

--- src/screens/InputScreen.js
+++ src/screens/InputScreen.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
 
 const InputScreen = () => {
+    const [name, setName] = useState('');
+
     return <View>
-        <Text>Enter name</Text>
-    </View>
+                <Text>Enter Name</Text>
+                <TextInput 
+                    style={styles.input}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={name}
+                    onChangeText={(newValue) => setName(newValue)}
+                />
+                <Text>My name is {name}</Text>
+            </View>
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1
+    }
+});
 
 export default InputScreen;
